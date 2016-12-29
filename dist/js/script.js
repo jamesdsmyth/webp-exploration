4574609fc45a565beeb053e8d3eafc098cb68dbb
James <smythie_89@hotmail.com>
2016-12-29 16:45:55 +0400

    Added all images and referenced firebase images

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+// (function() {
+//   // var images = document.querySelector('img').files;
+//   //
+//   // for (var i = 0; i < images.length; i++) {
+//   //   console.log(images[i])
+//   // }
+// })();
